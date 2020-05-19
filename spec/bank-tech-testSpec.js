5f1b44380c0b878ef7b75c79301cbe7a03ec5e47
Steven-Klavins <stevenklavins94@gmail.com>
2020-05-19 19:03:29 +0100

    Formatted code

--- spec/bank-tech-testSpec.js
+++ spec/bank-tech-testSpec.js
@@ -1,57 +1,61 @@
-'use strict';
+"use strict";
 
 var account;
 
-beforeEach(function() {
-  account = new Account()
-});
- 
-describe('Check Jasmine is working', function() {
-  it('It creates a new instance of the account function', function() {
-      expect(account).toEqual(account);
-  });
+beforeEach(function () {
+  account = new Account();
 });
 
-describe('User can make a deposite and withdraw money ', function() {
-  it('User adds 100', function() {
-    account.deposit(100)
-    expect(account.balance).toEqual(100.00);
+describe("Check Jasmine is working", function () {
+  it("It creates a new instance of the account function", function () {
+    expect(account).toEqual(account);
   });
-
-  it('User adds 100', function() {
-    account.deposit(100)
-    account.withdraw(60)
-    expect(account.balance).toEqual(40.00);
-  }); 
-
-  it('User adds 100', function() {
-    account.deposit(100.00)
-    account.withdraw(1000.00)
-    expect(account.balance === 100.00);
-  });
-
 });
 
-
-describe('User can see all their transactions', function() {
-
-  it('Shows one transaction', function() {
-    spyOn(console, 'log');
-    account.deposit(100.00)
-    account.showTransactions()
-    expect(console.log).toHaveBeenCalledWith("date || credit || debit || balance \n ")
-    expect(console.log).toHaveBeenCalledWith("19/05/2020" + " || 100.00 || 100.00")
+describe("User can make a deposite and withdraw money ", function () {
+  it("User adds 100", function () {
+    account.deposit(100);
+    expect(account.balance).toEqual(100.0);
   });
 
+  it("User adds 100", function () {
+    account.deposit(100);
+    account.withdraw(60);
+    expect(account.balance).toEqual(40.0);
+  });
+  it("User adds 100", function () {
+    account.deposit(100.0);
+    account.withdraw(1000.0);
+    expect(account.balance === 100.0);
+  });
+});
 
-  it('Shows multiple transactions', function() {
-    spyOn(console, 'log');
-    account.deposit(100.00)
-    account.withdraw(27.30)
-    account.showTransactions()
-    expect(console.log).toHaveBeenCalledWith("date || credit || debit || balance \n ")
-    expect(console.log).toHaveBeenCalledWith("19/05/2020" + " || 100.00 || 100.00")
-    expect(console.log).toHaveBeenCalledWith("19/05/2020" + " || 27.30 || 72.70")
+describe("User can see all their transactions", function () {
+  it("Shows one transaction", function () {
+    spyOn(console, "log");
+    account.deposit(100.0);
+    account.showTransactions();
+    expect(console.log).toHaveBeenCalledWith(
+      "date || credit || debit || balance \n "
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      "19/05/2020" + " || 100.00 || 100.00"
+    );
   });
 
+  it("Shows multiple transactions", function () {
+    spyOn(console, "log");
+    account.deposit(100.0);
+    account.withdraw(27.3);
+    account.showTransactions();
+    expect(console.log).toHaveBeenCalledWith(
+      "date || credit || debit || balance \n "
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      "19/05/2020" + " || 100.00 || 100.00"
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      "19/05/2020" + " || 27.30 || 72.70"
+    );
+  });
 });
--- src/bank-tech-test.js
+++ src/bank-tech-test.js
@@ -1,54 +1,49 @@
-'use strict';
+"use strict";
 
 function Account() {
-    this.balance = 0
-    this.date = new Date
-    this.listOfTransactions = []
+  this.balance = 0;
+  this.date = new Date();
+  this.listOfTransactions = [];
 }
 
 Account.prototype.deposit = function (val) {
- this.balance += val
- this.addTransaction(val) 
-}
+  this.balance += val;
+  this.addTransaction(val);
+};
 
 Account.prototype.withdraw = function (val) {
   if (val <= this.balance) {
-        this.balance -= val
-        this.addTransaction(val)      
-  } 
-  else {
-    console.log("Your balance is currently only " + (this.balance.toFixed(2)))
+    this.balance -= val;
+    this.addTransaction(val);
+  } else {
+    console.log("Your balance is currently only " + this.balance.toFixed(2));
   }
-}
+};
 
 Account.prototype.showTransactions = function () {
-    
-    
-    for (const value of this.listOfTransactions) {
-        var flag = true 
-        if (flag){
-            console.log("date || credit || debit || balance \n ")
-            flag = false
-        }
-        console.log(value);
-      }
-    
-}
+  for (const value of this.listOfTransactions) {
+    var flag = true;
+    if (flag === true) {
+      console.log("date || credit || debit || balance \n ");
+      flag = false;
+    }
+    console.log(value);
+  }
+};
 
-Account.prototype.format = function (){
-    var formattedDate = ('0' + this.date.getDate()).slice(-2) + '/'
-    + ('0' + (this.date.getMonth()+1)).slice(-2) + '/'
-    + this.date.getFullYear();
-    return formattedDate
-}
+Account.prototype.format = function () {
+  var formattedDate =
+    ("0" + this.date.getDate()).slice(-2) +
+    "/" +
+    ("0" + (this.date.getMonth() + 1)).slice(-2) +
+    "/" +
+    this.date.getFullYear();
+  return formattedDate;
+};
 
 Account.prototype.addTransaction = function (amount) {
-    var nDate = this.format()
-    var str = nDate + " || "+ amount.toFixed(2) +" || " + this.balance.toFixed(2)
-    this.listOfTransactions.push(str)
-}
-
-
-
-
-
+  var nDate = this.format();
+  var str =
+    nDate + " || " + amount.toFixed(2) + " || " + this.balance.toFixed(2);
+  this.listOfTransactions.push(str);
+};
