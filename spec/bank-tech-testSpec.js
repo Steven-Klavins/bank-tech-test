24fc9c092ca2f57dc80d8ae7658b6e23503a40f7
Steven <Steven@Stevens-iMac.local>
2020-05-19 13:10:56 +0100

    refactored

--- spec/bank-tech-testSpec.js
+++ spec/bank-tech-testSpec.js
@@ -1,38 +1,38 @@
 'use strict';
 
-var bank;
+var account;
 
 beforeEach(function() {
-  bank = new Bank()
+  account = new Account()
 });
  
 describe('Check Jasmine is working', function() {
-  it('It creates a new instance of the bank function', function() {
-      expect(bank).toEqual(bank);
+  it('It creates a new instance of the account function', function() {
+      expect(account).toEqual(account);
   });
 });
 
 describe('User can make a deposite and withdraw money ', function() {
   it('User adds 100', function() {
-    bank.deposit(100)
-    expect(bank.balance).toEqual(100);
+    account.deposit(100)
+    expect(account.balance).toEqual(100.00);
   });
 
   it('User adds 100', function() {
-    bank.deposit(100)
-    bank.withdraw(60)
-    expect(bank.balance).toEqual(40);
+    account.deposit(100)
+    account.withdraw(60)
+    expect(account.balance).toEqual(40.00);
   }); 
 
   it('User adds 100', function() {
-    bank.deposit(100)
-    bank.withdraw(1000)
-    expect(bank.balance === 100);
+    account.deposit(100.00)
+    account.withdraw(1000.00)
+    expect(account.balance === 100.00);
   });
 
-  it('It creates a new instance of the bank function', function() {
-    bank.deposit(100)
-    expect(bank.showTransactions()).toEqual("date || credit || debit || balance \n "+ "19/05/2020" + " || || 100.00 || 100.00")
+  it('It creates a new instance of the account function', function() {
+    account.deposit(100.00)
+    expect(account.showTransactions()).toEqual("date || credit || debit || balance \n "+ "19/05/2020" + " || 100.00 || 100.00")
 });
 });
 
--- src/bank-tech-test.js
+++ src/bank-tech-test.js
@@ -1,30 +1,32 @@
 'use strict';
 
-function Bank() {
+function Account() {
     this.balance = 0
     this.date = new Date
+    this.listOfTransactions = []
 }
 
-Bank.prototype.deposit = function (val) {
+Account.prototype.deposit = function (val) {
  this.balance += val
 }
 
-Bank.prototype.withdraw = function (val) {
+Account.prototype.withdraw = function (val) {
   if (val <= this.balance) {
         this.balance -= val
-  }
+  } 
   else {
-    console.log("Your balance is currently only " + this.balance)
+    console.log("Your balance is currently only " + (this.balance.toFixed(2)))
   }
 }
 
-Bank.prototype.showTransactions = function () {
+Account.prototype.showTransactions = function () {
     var formatDate = this.format()
-    console.log ("date || credit || debit || balance \n "+ formatDate + " || || 100.00 || 100.00")
-    return "date || credit || debit || balance \n "+ formatDate + " || || 100.00 || 100.00"
+    var num = this.balance.toFixed(2)
+    console.log ("date || credit || debit || balance \n "+ formatDate + " || 100.00 || 100.00")
+    return "date || credit || debit || balance \n "+ formatDate + " || "+num+" || 100.00"
 }
 
-Bank.prototype.format = function (){
+Account.prototype.format = function (){
     var formattedDate = ('0' + this.date.getDate()).slice(-2) + '/'
     + ('0' + (this.date.getMonth()+1)).slice(-2) + '/'
     + this.date.getFullYear();
