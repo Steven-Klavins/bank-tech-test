1a04c25fc59729c61b91173a00a5fc05bb51840a
Steven-Klavins <stevenklavins94@gmail.com>
2020-05-19 05:25:30 +0100

    User can now make a deposite

--- spec/bank-tech-testSpec.js
+++ spec/bank-tech-testSpec.js
@@ -1,12 +1,21 @@
 'use strict';
 
-describe('bank-tech-test', function() {
+var bank;
 
-  describe('Check Jasmine is working', function() {
-    var bank = new Bank();
-    it('It creates a new instance of the bank function', function() {
-      expect(bank).toEqual(bank);
-    });
-  });
-
+beforeEach(function() {
+  bank = new Bank()
 });
+ 
+describe('Check Jasmine is working', function() {
+  it('It creates a new instance of the bank function', function() {
+      expect(bank).toEqual(bank);
+  });
+});
+
+describe('User can make a deposite', function() {
+  it('User adds 100', function() {
+    bank.deposit(100)
+    expect(bank.balance).toEqual(100);
+  });
+});
+
