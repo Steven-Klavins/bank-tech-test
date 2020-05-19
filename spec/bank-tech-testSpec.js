c49e86c264bf76f0943a242964fd1701145abfbe
Steven-Klavins <stevenklavins94@gmail.com>
2020-05-19 06:38:18 +0100

    Refactored tests

--- spec/bank-tech-testSpec.js
+++ spec/bank-tech-testSpec.js
@@ -12,27 +12,27 @@ describe('Check Jasmine is working', function() {
   });
 });
 
-describe('User can make a deposite', function() {
+describe('User can make a deposite and withdraw money ', function() {
   it('User adds 100', function() {
     bank.deposit(100)
     expect(bank.balance).toEqual(100);
   });
-});
 
-
-describe('User can withdraw money', function() {
   it('User adds 100', function() {
     bank.deposit(100)
     bank.withdraw(60)
     expect(bank.balance).toEqual(40);
+  }); 
+
+  it('User adds 100', function() {
+    bank.deposit(100)
+    bank.withdraw(1000)
+    expect(bank.balance === 100);
   });
 });
 
+
 describe('User can withdraw money providing they dont request more than their balence', function() {
-  it('User adds 100', function() {
-    bank.deposit(100)
-    bank.withdraw(101)
-    expect(bank.balance === 100);
-  });
+ 
 });
 
