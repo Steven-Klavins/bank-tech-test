c6f9018c97546be65e188647a99536bf1a4bc1de
Steven-Klavins <stevenklavins94@gmail.com>
2020-05-22 11:27:18 +0100

    Changed tests to mock dates differently

--- spec/bank-tech-testSpec.js
+++ spec/bank-tech-testSpec.js
@@ -3,23 +3,27 @@
 let account;
 
 beforeEach(function () {
-  account = new Account();
-  let today = Date("2020-05-20");
-  jasmine.clock().mockDate(today);
+  var oldDate = new Date("2020-05-20");
+  spyOn(window, "Date").and.callFake(function () {
+    return oldDate;
+  });
 });
 
 describe("User can make a deposit and withdraw money ", () => {
   it("User adds 100", function () {
+    account = new Account();
     account.deposit(100);
-    expect(account.balance).toEqual(100.0);
+    expect(account.balance).toEqual(100);
   });
 
   it("User adds 100", () => {
+    account = new Account();
     account.deposit(100);
     account.withdraw(60);
     expect(account.balance).toEqual(40.0);
   });
   it("User adds 100", () => {
+    account = new Account();
     account.deposit(100.0);
     account.withdraw(1000.0);
     expect(account.balance === 100.0);
@@ -28,12 +32,14 @@ describe("User can make a deposit and withdraw money ", () => {
 
 describe("The date is converted to the correct format", () => {
   it("Shows the date correctly formatted", () => {
+    account = new Account();
     expect(account.format === "20/05/2020");
   });
 });
 
 describe("User can see all their transactions", () => {
   it("Shows one transaction", () => {
+    account = new Account();
     spyOn(console, "log");
     account.deposit(100.0);
     account.showTransactions();
@@ -47,6 +53,7 @@ describe("User can see all their transactions", () => {
 
   it("Shows multiple transactions", () => {
     spyOn(console, "log");
+    account = new Account();
     account.deposit(100.0);
     account.withdraw(27.3);
     account.showTransactions();
